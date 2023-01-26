27b9a171552ca87694add4e2d4bd8978498872be
Emily Bishop <emilybishop@Emilys-MacBook-Pro-3.local>
2023-01-26 09:33:21 -0800

    added tables for national and regional summaries

--- R_scripts/03_data_tables.R
+++ R_scripts/03_data_tables.R
@@ -1,7 +1,8 @@
 #Written by Emily Bishop
-#Last updated January 25, 2023
+#Last updated January 26, 2023
 
 #### load packages ####
+library(tidyverse)
 library(janitor)
 library(gt)
 
@@ -10,12 +11,12 @@ source("R_scripts/02_summarize_data.R")
 
 #### Table 2 ####
 #calculate frequency for each  management category recommended nationally
-nat_mgmt <- resilience_data %>% 
+nat_mgmt <- resilience_df %>% 
   group_by(ManagementCategory) %>% 
   summarize(n = n())
 
 #split the main dataframe by region
-mgmt_reg_list <- resilience_data %>% 
+mgmt_reg_list <- resilience_df %>% 
   group_split(HUC_Region)
 
 #create an empty matrix to store frequency data for each region
@@ -33,7 +34,7 @@ for (i in 1:5) {
 reg_mgmt <- reg_mgmt %>% as.data.frame() %>% rename("Gulf of Mexico" = 1, "Mid-Atlantic" = 2, "Northeast" = 3, "Southeast" = 4, "West Coast" = 5)
 
 #create row names
-mgmt_labs <- resilience_data %>% 
+mgmt_labs <- resilience_df %>% 
   distinct(ManagementCategory) %>% 
   arrange(ManagementCategory) #matches order from the nat and reg summaries
 
@@ -79,3 +80,64 @@ mgmt_sum <- bind_cols(mgmt_labs, reg_mgmt, Total = nat_mgmt$n) %>%
 mgmt_sum 
 
 #gtsave(mgmt_sum, "outputs/mgmt_sum.png", expand = 10)
+
+#### S1 National Summary ####
+nat_mean_2 <- as.data.frame(apply(resilience_ntiles, 2, meansd))
+names(nat_mean_2) <- "Avg_Resilience_Score"
+
+summary_metrics <- gsub("_", " ", summary_metrics)
+nat_mean_2$Metrics <- summary_metrics
+
+national_table <- nat_mean_2 %>% 
+  gt(rowname_col = "Metrics") %>% 
+  cols_label(Avg_Resilience_Score = "Average Resilience Score") %>% 
+  cols_align(
+    align = "center",
+    columns = "Avg_Resilience_Score") %>% 
+  tab_options(
+    table.font.name = "Calibri",
+    table.font.color = "black",
+    table.border.top.style = "none",
+    table.border.bottom.style = "solid",
+    table.border.bottom.color = "black",
+    table.border.bottom.width = px(3),
+    column_labels.border.bottom.color = "black",
+    column_labels.border.bottom.width = px(3),
+    data_row.padding = px(10)) %>% 
+  opt_row_striping() %>% 
+  tab_row_group(label = "Total", 
+                rows = 17) %>% 
+  tab_row_group(label = "Metric Categories", 
+                rows = c(14:16)) %>% 
+    tab_row_group(label = "Metrics", 
+                  rows = c(1:13)) ; national_table
+
+#### S1 Regional Summary ####
+S1_reg_sum <- t(reg_mean_sd) %>% 
+  row_to_names(row_number = 1) %>% 
+  as.data.frame() %>% 
+  mutate(Metrics = summary_metrics)
+
+regional_table <- S1_reg_sum %>% 
+  gt(rowname_col = "Metrics") %>% 
+  cols_align(
+    align = "center",
+    columns = !Metrics) %>%
+  tab_options(
+    table.font.name = "Calibri",
+    table.font.color = "black",
+    table.border.top.style = "none",
+    table.border.bottom.style = "solid",
+    table.border.bottom.color = "black",
+    table.border.bottom.width = px(3),
+    column_labels.border.bottom.color = "black",
+    column_labels.border.bottom.width = px(3),
+    data_row.padding = px(10)) %>% 
+  opt_row_striping() %>% 
+  tab_row_group(label = "Total", 
+                rows = 17) %>% 
+  tab_row_group(label = "Metric Categories", 
+                rows = c(14:16)) %>% 
+  tab_row_group(label = "Metrics", 
+                rows = c(1:13)) ; regional_table
+
